16120c588d57195e54fa67ab17647c170b9f3a5b
Reesha HR <727724eucs211@skcet.ac.in>
2025-12-28 14:16:10 +0530

    Update TestAspect.java

--- springapp/src/main/java/com/example/railwaystationissuetrackingsystem/aop/TestAspect.java
+++ springapp/src/main/java/com/example/railwaystationissuetrackingsystem/aop/TestAspect.java
@@ -8,16 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserAopAspect {
 
-    @Around(
-      "execution(* com.example.railwaystationissuetrackingsystem.service.UserService.getAllUsers(..))"
-    )
+    @Around("execution(* com.example.railwaystationissuetrackingsystem.service.UserService.getAllUsers(..))")
     public Object aroundGetAllUsers(ProceedingJoinPoint pjp) throws Throwable {
-
-        System.out.println("AOP BEFORE getAllUsers()");
-
+       
+        System.out.println("AOP LOG: Intercepting request to fetch all users...");
         Object result = pjp.proceed();
-
-        System.out.println("AOP AFTER getAllUsers()");
+        System.out.println("AOP LOG: Successfully fetched users. Result: " + result);
 
         return result;
     }
